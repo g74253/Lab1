ad77334c6e21f9da18d9089e9976d63b84b02848
hazielgr <mariohaziel@gmail.com>
2024-02-20 19:42:24 -0600

    Se crea sumador completo de 1BIT
    Se crea sumador completo de 1bit para ser utilizado como base para el sumador completo de 4 bits

---
+++ laboratorio_1/problema_2/Sumador_1bit.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Sumador_1bit is
+    Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           Cin : in STD_LOGIC;
+           Sum : out STD_LOGIC;
+           Cout : out STD_LOGIC);
+end Sumador_1bit;
+
+architecture Behavioral of Sumador_1bit is
+
+begin
+
+    Sum <= A xor B xor Cin;
+    Cout <= (A and B) or (B and Cin) or (A and Cin);
+
+end Behavioral;
