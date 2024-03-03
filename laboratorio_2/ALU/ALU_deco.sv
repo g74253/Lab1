116f717e38ff4462e8a457718f10c8e584406a2c
g74253 <38565604+g74253@users.noreply.github.com>
2024-03-02 18:11:40 -0600

    estructura case de la ALU

---
+++ laboratorio_2/ALU/ALU_deco.sv
@@ -0,0 +1,11 @@
+module ALU_deco #(parameter n=4)
+					 (input logic [n-1:0] entrada1,  
+					  input logic [n-1:0] entrada2, 
+					  input logic [3:0] selector, 
+					  output logic [6:0]salida);
+					  
+logic [3:0]resultado);
+ALU_case calculadora(entrada1,entrada2,selector,resultado);
+
+					  
+endmodule
