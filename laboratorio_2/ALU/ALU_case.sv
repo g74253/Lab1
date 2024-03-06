dbf8708dda853b9084540a139f18d28cdb4cf9ef
hazielgr <mariohaziel@gmail.com>
2024-03-06 00:21:29 -0600

    Modulo
    Se agrega operacion modulo y se agrega al alu case, se arregla error en operacion multiplicacion

--- laboratorio_2/ALU/ALU_case.sv
+++ laboratorio_2/ALU/ALU_case.sv
@@ -19,10 +19,19 @@ module ALU_case #(parameter n=4)
 	logic temp_negativo_mult;
 	logic temp_cero_mult;
 	logic temp_desbordamiento_mult;	
+	
+	//Modulo
+	logic [3:0] temp_resultado_mod:
+	logic temp_carry_mod;
+	logic temp_negativo_mod;
+	logic temp_cero_mod;
+	logic temp_desbordamiento_mod;	
 
 	suma_parametrizable suma (entrada1,entrada2,temp_resultado_suma,temp_carry_suma,temp_cero_suma);
 	
-	multiplicacion mult (entrada1, entrada2, temp_resultado_mult, temp_carry_mult, temp_negativo_mult, temp_cero_mult, temp_desbordamiento_mult); 
+	multiplicacion mult (entrada1, entrada2, temp_resultado_mult, temp_carry_mult, temp_negativo_mult, temp_desbordamiento_mult, temp_cero_mult); 
+	
+	mod modulo (entrada1, entrada2, temp_resultado_mod, temp_carry_mod, temp_negativo_mod, temp_desbordamiento_mod, temp_cero_mod)
 	
 	always @(selector,entrada1,entrada2)
 	begin
@@ -49,6 +58,11 @@ module ALU_case #(parameter n=4)
 				end
 			4'b0100:// modulo
 				begin
+				resultado = temp_resultado_mod;
+				carry = temp_carry_mod;
+				cero = temp_cero_mod;
+				negativo = temp_negativo_mod;
+				desbordamiento = temp_desbordamiento_mod;
 				end
 			4'b0101:// and
 				begin
