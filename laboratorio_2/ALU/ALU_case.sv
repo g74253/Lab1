c8b007ac21666834922cdac4106c12e52c1765d3
hazielgr <mariohaziel@gmail.com>
2024-03-05 23:26:54 -0600

    Multiplicacion
    Se agrega operacion de multiplicacion y se agrega case de la ALU

--- laboratorio_2/ALU/ALU_case.sv
+++ laboratorio_2/ALU/ALU_case.sv
@@ -4,13 +4,25 @@ module ALU_case #(parameter n=4)
 					  input logic [3:0] selector, 
 					  output logic [3:0]resultado,
 					  output logic carry, 
-					  output logic cero);
+					  output logic cero,
+					  output logic negativo,
+					  output logic desbordamiento);
 	
 	logic [3:0] temp_resultado_suma;
 	logic temp_carry_suma;
 	logic temp_cero_suma;
+	
+	
+	//Multiplicacion
+	logic [3:0] temp_resultado_mult;
+	logic temp_carry_mult;
+	logic temp_negativo_mult;
+	logic temp_cero_mult;
+	logic temp_desbordamiento_mult;	
 
 	suma_parametrizable suma (entrada1,entrada2,temp_resultado_suma,temp_carry_suma,temp_cero_suma);
+	
+	multiplicacion mult (entrada1, entrada2, temp_resultado_mult, temp_carry_mult, temp_negativo_mult, temp_cero_mult, temp_desbordamiento_mult); 
 	
 	always @(selector,entrada1,entrada2)
 	begin
@@ -26,6 +38,11 @@ module ALU_case #(parameter n=4)
 				end
 			4'b0010: //aqui va multiplicacion
 				begin
+				resultado = temp_resultado_mult;
+				carry = temp_carry_mult;
+				cero = temp_cero_mult;
+				negativo = temp_negativo_mult;
+				desbordamiento = temp_desbordamiento_mult;
 				end
 			4'b0011://aqui va divicion
 				begin
