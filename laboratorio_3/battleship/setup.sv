cffc672672ffc4037e55b25993eeaa7aab0d3a1c
g74253 <38565604+g74253@users.noreply.github.com>
2024-04-15 19:01:08 -0600

    battleship
    Fsm, contador de 15 seg y estructuctura de setup.

---
+++ laboratorio_3/battleship/setup.sv
@@ -0,0 +1,14 @@
+module setup(input logic end_setup
+				input logic boton_arriba,
+				input logic boton_abajo,
+				input logic boton_izquierda,
+				input logic boton_derecha,
+				input logic [2:0] cant_barco,
+				input logic matriz,
+				input logic boton_colocar);
+				
+				
+				
+	always @()begin
+	end
+endmodule
